ea987decd979238176e725ba0cd7066a2da988b3
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-02 12:47:40 +0200

    refactor script: for loop for grid creation now start from 1 to 256

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sketchContainer = document.querySelector('.sketch-container');
 
 //Crée une grille de 16x16
-for(i=0;i<256;i++){
+for(i=1;i<=256;i++){
    let div = document.createElement('div');
    div.setAttribute('id',''+i+'');
    sketchContainer.appendChild(div);

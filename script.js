4b14abce4d79484a462f1296128268b4a376605e
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-02 17:40:57 +0200

    feat: add the possibility to click on a square zone to color it in black

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const sketchContainer = document.querySelector('.sketch-container');
 
-//Crée une grille de 16x16
+// Crée une grille de 16x16
 for(i=1;i<=256;i++){
    let div = document.createElement('div');
    div.setAttribute('id',''+i+'');
    sketchContainer.appendChild(div);
 }
+
+// Click sur une zone pour colorier la zone en noir
+sketchContainer.addEventListener('mouseenter',() => {
+   let squares = document.querySelectorAll('.sketch-container > div');
+   squares.forEach(square => {
+      square.addEventListener('click',() => {
+         square.style.background = 'black';
+      });
+   });
+});

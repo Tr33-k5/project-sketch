556d97d0495fe39c799f53b7af24d3c59faa51ac
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-03 16:40:19 +0200

    feat: hold the mouse pressed to continue to color the canvas

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const sketchContainer = document.querySelector('.sketch-container');
+// Disable drag-and-drop feature on the canvas
+document.querySelector('.sketch-container').ondragstart = function() {
+   return false;
+};
 
-// Crée une grille de 16x16
+// Create a 16x16 grid
 for(i=1;i<=256;i++){
    let div = document.createElement('div');
    div.setAttribute('id',''+i+'');
    sketchContainer.appendChild(div);
 }
 
-// Click sur une zone pour colorier la zone en noir
+/* Click on cell to color it in black */
+// Mouse pointer enter the grid
 sketchContainer.addEventListener('mouseenter',() => {
    let squares = document.querySelectorAll('.sketch-container > div');
    squares.forEach(square => {
-      square.addEventListener('mousedown',(e) => {
-         // Click gauche de la souris
-         if(e.button===0){
+      square.addEventListener('mousemove',(e) => {
+         // If no mouse click or press
+         if(e.buttons === 0){ return }
+         // On left mouse click
+         if(e.button === 0){
             square.style.background = 'black';
          }
       });

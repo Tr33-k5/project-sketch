20463b32eaea5df7b2ab6d67809696dc11fb913f
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-04 03:36:09 +0200

    refactor: use of a function to create the canvas instead of creating it manually

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const sketchContainer = document.querySelector('.canvas');
+let canvas = document.querySelector('.canvas');
 // Disable drag-and-drop feature on the canvas
 document.querySelector('.canvas').ondragstart = function() {
    return false;
 };
 
-// Create a 16x16 grid
-for(i=1;i<=256;i++){
-   let div = document.createElement('div');
-   div.setAttribute('id',''+i+'');
-   sketchContainer.appendChild(div);
-}
+/* Create a 16x16 canvas */
+createCanvas(16);
 
 /* Click on cell to color it in black */
 // Mouse pointer hover the canvas
 let squares = document.querySelectorAll('.canvas > div');
 squares.forEach(square => {
-   square.addEventListener('mousedown',(e) => {
+      square.addEventListener('mousedown',(e) => {
       // If no mouse click or press
       if(e.buttons === 0){ return }
       // On left mouse click
@@ -32,3 +28,26 @@ squares.forEach(square => {
       }
    });
 });
+
+// Create canvas with display grid css styles
+function createCanvas(res){
+   const sheet = new CSSStyleSheet();
+   sheet.replaceSync(".canvas {"+
+      "grid-template-columns: repeat("+res+", 6.25%);"+
+      "grid-template-rows: repeat("+res+", 6.25%);"+
+   "}");
+   document.adoptedStyleSheets = [sheet];
+   for(i=1;i<=(res*res);i++){
+      let div = document.createElement('div');
+      div.setAttribute('id',''+i+'');
+      canvas.appendChild(div);
+   }
+}
+
+// Erase canvas
+function removeCanvas(){
+   let divs = document.querySelector('.canvas > div');
+   divs.forEach(div => {
+      div.remove();
+   });
+}
